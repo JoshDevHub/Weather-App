04538fdd84a8f40d7b6c87e3ad7bf081c34e420b
Josh Smith <jmsmith1018@gmail.com>
2023-02-19 14:32:26 -0700

    feat: hook into search form for getting weather

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import "./stylesheets/application.scss";
 import * as Location from "./get_location";
 import * as Weather from "./get_weather";
 
-const newEl = document.createElement("div");
-newEl.classList.add("blue");
-newEl.textContent = "ooga";
-document.body.appendChild(newEl);
+const form = document.querySelector("form");
+const searchInput = document.getElementById("search");
+const handleSearch = (event) => {
+  event.preventDefault();
 
-Location
-  .fetchData("London")
-  .then(Weather.fetchData)
-  .then(console.log);
+  const searchTerm = searchInput.value;
+  Location.fetchData(searchTerm).then(Weather.fetchData).then(console.log);
+
+  searchInput.value = "";
+}
+
+form.addEventListener("submit", handleSearch);
