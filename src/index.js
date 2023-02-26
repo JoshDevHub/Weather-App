49d9110f8b4704e872573bc3462c9b912c6e762d
Josh Smith <jmsmith1018@gmail.com>
2023-02-26 10:00:24 -0700

    feat: use `localStorage` to store latest query

--- src/index.js
+++ src/index.js
@@ -7,21 +7,39 @@ import * as Weather from "./get_weather";
 import { renderWeather } from "./render_weather";
 import { renderError } from "./render_error";
 
+function getLatestQuery() {
+  return localStorage.getItem("lastQuery");
+}
+
+const saveQuery = (data) => {
+  localStorage.setItem("lastQuery", data.name);
+}
 
 const form = document.querySelector("form");
 const searchInput = document.getElementById("search");
 const unitToggle = document.getElementById("units");
 const getUnitSystem = () => unitToggle.checked ? "metric" : "imperial";
 
+const queryAndRenderWeather = (searchTerm) => {
+  Location
+    .fetchData(searchTerm)
+    .then((location) => Weather.fetchData(location, getUnitSystem()))
+    .then((weatherData) => {
+      saveQuery(weatherData);
+      renderWeather(weatherData);
+    })
+    .catch(renderError)
+}
+
+queryAndRenderWeather(
+  getLatestQuery() ?? "London"
+)
+
 const handleSearch = (event) => {
   event.preventDefault();
 
   const searchTerm = searchInput.value;
-  Location
-    .fetchData(searchTerm)
-    .then((location) => Weather.fetchData(location, getUnitSystem()))
-    .then(renderWeather)
-    .catch(renderError)
+  queryAndRenderWeather(searchTerm);
 
   searchInput.textContent = "";
 }
